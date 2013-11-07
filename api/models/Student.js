05f8dcb51f67e43e6c2c6736ac60b667ecb143d5
crzrcn <fernanlink@gmail.com>
2013-11-07 18:12:05 -0400

    Added Student model.

---
+++ api/models/Student.js
@@ -0,0 +1,21 @@
+/**
+ * Student
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  tableName: 'uprrp_ges_students',
+
+  attributes: {
+    
+    student_number	: {
+      type: 'INTEGER',
+      required: true
+    }
+  }
+
+};
