eee161e743dc7a91d7dc33f6a492528eab10ae21
crzrcn <fernanlink@gmail.com>
2013-11-07 18:11:52 -0400

    Added Grade  model.

---
+++ api/models/Grade.js
@@ -0,0 +1,31 @@
+/**
+ * Grade
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+  tableName: 'uprrp_ges_grades',
+
+  attributes: {
+    
+    course_id	: {
+      type: 'INTEGER',
+      required: true
+    },
+
+    student_id	: {
+      type: 'INTEGER',
+      required: true
+    },
+
+    grade	: {
+      type: 'STRING',
+      in: ['A', 'B', 'C', 'D', 'F']
+    }
+  }
+
+};
