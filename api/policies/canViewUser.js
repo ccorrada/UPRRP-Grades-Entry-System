c5afb6d4338a1f5a78bf90028af194c3b15d84fa
crzrcn <fernanlink@gmail.com>
2013-11-30 17:51:05 -0400

    Server no longer crashes when /user/:userId is requested with an invalid userId.

--- api/policies/canViewUser.js
+++ api/policies/canViewUser.js
@@ -1,12 +1,13 @@
 module.exports = function (req, res, ok) {
-  User.findOne(req.param('id'), function (err, user) {
-    if (err) {
-      console.log(err);
-    } else if (user.length !== 0) {
+  User.findOne().where({id: req.param('id')}).then(function (user) {
+    if (user) {
       if (parseInt(req.param('id')) === user.id)
         ok();
-      else
-        return res.send('I\'m sorry, but I cannot let you do that.', 403);
+    } else {
+      return res.send('I\'m sorry, but I cannot let you do that.', 403);
     }
+  }).fail(function (err) {
+    console.log(err);
+    res.redirect('/');
   });
 }
